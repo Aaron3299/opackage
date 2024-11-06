b2c5ff1ea6c98de91961fc2f5e259781887214ff
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-11-06 13:04:06 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -35,17 +35,7 @@ git clone --depth 1 https://github.com/ximiTech/luci-app-msd_lite
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
 git clone --depth 1 https://github.com/immortalwrt/packages && mv -n packages/net/{vsftpd,transmission} ./ ; rm -rf packages
 git clone --depth 1 https://github.com/immortalwrt/luci && mv -n luci/applications/{luci-app-vlmcsd,luci-app-transmission} ./ ; rm -rf luci
-#git_sparse_clone master "https://github.com/immortalwrt/packages" "immpack" net/sub-web net/dnsproxy net/haproxy net/cdnspeedtest net/subconverter net/ngrokc net/oscam net/njitclient net/scutclient net/udp2raw \
-#admin/btop libs/wxbase libs/rapidjson libs/libcron libs/quickjspp libs/toml11 libs/libtorrent-rasterbar \
-#libs/libdouble-conversion libs/qt6base libs/cxxopts libs/jpcre2 libs/alac \
-#utils/cpulimit devel/gn
 
-#git_sparse_clone master "https://github.com/immortalwrt/immortalwrt" "immortal" package/network/utils/nftables \
-#package/network/utils/fullconenat package/network/utils/fullconenat-nft \
-#package/utils/mhz package/libs/libnftnl package/firmware/wireless-regdb
-
-#sed -i '65,73d' adguardhome/Makefile
-sed -i 's/PKG_SOURCE_DATE:=2/PKG_SOURCE_DATE:=3/' transmission-web-control/Makefile
 find . -type f -name "update.sh" -exec rm -f {} \;
 rm -rf adguardhome/patches
 exit 0
