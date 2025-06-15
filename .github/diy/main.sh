34633eb943348188f80179248ce204aa3d573ffd
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2025-06-15 18:43:04 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -16,15 +16,10 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-#git clone --depth 1 https://github.com/sbwml/luci-app-qbittorrent openwrt-qb && mv -n openwrt-qb/* ./ ; rm -rf openwrt-qb
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall passwall1 && mv -n passwall1/luci-app-passwall  ./; rm -rf passwall1
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 passwall2 && mv -n passwall2/luci-app-passwall2 ./;rm -rf passwall2
 #git clone --depth 1 https://github.com/ximiTech/luci-app-msd_lite
 #git clone --depth 1 https://github.com/ximiTech/msd_lite
-#git clone --depth 1 https://github.com/kenzok78/luci-app-adguardhome
 #git clone --depth 1 https://github.com/immortalwrt/luci && mv -n luci/applications/{luci-app-msd_lite,luci-app-transmission} ./ ; rm -rf luci
 #git clone --depth 1 https://github.com/immortalwrt/packages && mv -n packages/net/{msd_lite,transmission-web-control,transmission} ./ ; rm -rf packages
-#git clone --depth 1 https://github.com/kiddin9/kwrt-packages && mv -n kwrt-packages/luci-app-bypass ./ ; rm -rf kwrt-packages
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages && mv -n openwrt-packages/{smartdns,luci-app-smartdns} ./ ; rm -rf openwrt-packages
 find . -type f -name "update.sh" -exec rm -f {} \;
 exit 0
